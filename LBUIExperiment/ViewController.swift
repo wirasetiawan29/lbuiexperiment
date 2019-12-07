002b60806ccfb0ff170bfeab47bd5318d71ea216
wira <wirasetiawan29@gmail.com>
2019-12-07 17:57:37 +0700

    init

---
+++ LBUIExperiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBUIExperiment
+//
+//  Created by wira on 12/7/19.
+//  Copyright © 2019 Wira Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
